68582ebdffc25f0c9d5911ad8d497ba6cf85a40e
ilya_k <ivk68@list.ru>
2023-04-03 21:45:04 +0300

    next moving

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import os
 # функция для импорта данных из текстового файла
+
 def import_data():
-    filename = 'contacts.txt'
-    if os.path.exists(filename):
-        with open(filename, 'r') as f:
+    filesurname = 'contacts.txt'
+    if os.path.exists(filesurname):
+        with open(filesurname, 'r') as f:
             data = f.readlines()
         return data
     else:
         print('Телефонный справочник не найден')
         return []
-
+import codecs
 # функция для экспорта данных в текстовый файл
 def export_data(data):
-    with open('contacts.txt', 'w') as f:
+    with codecs.open('contacts.txt', 'w', encoding='utf-8') as f:
         f.writelines(data)
     print('Данные успешно экспортированы')
 
 # функция для добавления новой записи в справочник
 def add_contact():
-    last_name = input('Введите фамилию: ')
-    first_name = input('Введите имя: ')
+    surname = input('Введите фамилию: ')
+    name = input('Введите имя: ')
     patronymic = input('Введите отчество: ')
     phone_number = input('Введите номер телефона: ')
-    new_contact = f'{last_name} {first_name} {patronymic} {phone_number}\n'
+    new_contact = f'{surname} {name} {patronymic} {phone_number}\n'
     phone_book.append(new_contact)
     print('Контакт успешно добавлен')
 
@@ -45,12 +46,12 @@ def edit_contact():
     search_query = input('Введите фамилию или имя человека для изменения: ')
     for i, contact in enumerate(phone_book):
         if search_query in contact:
-            last_name, first_name, patronymic, phone_number = contact.split(' ')
-            new_last_name = input(f'Введите новую фамилию (было {last_name}): ') or last_name
-            new_first_name = input(f'Введите новое имя (было {first_name}): ') or first_name
+            surname, name, patronymic, phone_number = contact.split(' ')
+            new_surname = input(f'Введите новую фамилию (было {surname}): ') or surname
+            new_name = input(f'Введите новое имя (было {name}): ') or name
             new_patronymic = input(f'Введите новое отчество (было {patronymic}): ') or patronymic
             new_phone_number = input(f'Введите новый номер телефона (был {phone_number}): ') or phone_number
-            new_contact = f'{new_last_name} {new_first_name} {new_patronymic} {new_phone_number}\n'
+            new_contact = f'{new_surname} {new_name} {new_patronymic} {new_phone_number}\n'
             phone_book[i] = new_contact
             print('Контакт успешно изменен')
     else:
@@ -105,4 +106,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
